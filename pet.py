bc26a90775d232e7b195e441740a9da1f5539d9d
Rodrigo Ramos <lyonn@mictlan.gluo.mx>
2017-07-21 17:42:47 -0500

    first pet

---
+++ pet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+message = raw_input('Ingresa tu mensaje: ')
+text_l = len(message)
+print ('-'*text_l)
+print ('<{}>'.format(message))
+print ('-'*text_l)
+print (' /')
+print (' /')
+print (' /\___/\\')
+print (' () | ()')
+print (' (     )')
+print ('  V---V')
